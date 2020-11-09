f917033926e70d2800a2eb03139aeacd45425b8b
Andreas Schröpfer <andreas.schroepfer@gmail.com>
2020-11-09 13:52:35 +0100

    #2 sum added to session view

--- cmd/wdgo/session.go
+++ cmd/wdgo/session.go
@@ -23,6 +23,7 @@ func (a *app) renderSession() {
 	activeBoard := &a.boards[a.activeBoard]
 	activeStage := activeBoard.board.Stages[activeBoard.activeStage]
 	activeCard := activeStage.Cards[activeBoard.activeCard]
+	var sum time.Duration
 	for i := range activeCard.Sessions {
 		s := activeCard.Sessions[len(activeCard.Sessions)-i-1]
 		a.card.sessions.SetCell(i, 0,
@@ -32,8 +33,13 @@ func (a *app) renderSession() {
 				tview.NewTableCell(s.End.Format(wdgo.TimeFormat)))
 			a.card.sessions.SetCell(i, 2,
 				tview.NewTableCell(s.End.Sub(s.Start).String()))
+			sum += s.End.Sub(s.Start)
 		}
 	}
+	a.card.sessions.SetCell(len(activeCard.Sessions), 2,
+		tview.NewTableCell("sum:"))
+	a.card.sessions.SetCell(len(activeCard.Sessions), 2,
+		tview.NewTableCell(sum.String()))
 	a.card.sessions.SetBorder(true)
 	a.card.sessions.SetBorders(true)
 }
